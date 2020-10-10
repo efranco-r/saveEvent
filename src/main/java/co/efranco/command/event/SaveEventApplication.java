087a5f453956e50ddf30217d50fe5ae63d64e513
Eduardo Franco <eduardo.franco.rivera@gmail.com>
2020-10-09 21:43:59 -0500

    New project to persist event within Hibernate

---
+++ src/main/java/co/efranco/command/event/SaveEventApplication.java
@@ -0,0 +1,13 @@
+package co.efranco.command.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/efranco/command/event/SaveEventApplicationTests.java
@@ -0,0 +1,13 @@
+package co.efranco.command.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
